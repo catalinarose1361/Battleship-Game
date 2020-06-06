380b322d0bb74fe717ebee8439fb58b4905736c2
Catalina Williams <catalina.rose1361@gmail.com>
2020-06-06 17:55:56 -0600

    logic built out

--- battleship.js
+++ battleship.js
@@ -28,9 +28,23 @@ while (isSunk === false) {
         guesses = guesses + 1
     }
         // if the users guess matches the location
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert("HIT!")
             // add one to the number of hits
+            hits = hits + 1
             // if the number of hits is 3 
+            if (hits == 3) {
                 // set isSunk to true
+                isSunk = true
                 // tell user "You sank my battleship"
+                alert("You sank my battleship!!!")
+
+            }
+        } else {
+            alert("MISS")
+        } 
 }
 // tell user stats
+var stats = "You took " + guesses + "guesses to sink the battleship, " + "which means your shooting accuracy was " + (3/guesses)
+
+alert(stats)
